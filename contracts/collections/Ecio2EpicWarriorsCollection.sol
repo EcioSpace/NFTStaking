d0bdd7c535eda115491f3cda4034b24a63b78845
Artizno <artiz@ecio.space>
2022-01-19 21:43:22 +0700

    Add new collection

--- contracts/collections/Ecio2EpicWarriorsCollection.sol
+++ contracts/collections/Ecio2EpicWarriorsCollection.sol
@@ -6,10 +6,24 @@ import "../helper/IECIONFT.sol";
 import "../helper/EcioChallenge.sol";
 import "hardhat/console.sol";
 
-contract Ecio2EpicWarriorsCollection is EcioChallenge {
-
+contract Ecio2EpicWarriorsCollection is EcioChallenge, ECIOHelper {
     IECIONFT nftCoreV1Contract;
     IECIONFT nftCoreV2Contract;
+
+    // Part Code Index
+    uint256 constant PC_NFT_TYPE = 12;
+    uint256 constant PC_KINGDOM = 11;
+    uint256 constant PC_CAMP = 10;
+    uint256 constant PC_GEAR = 9;
+    uint256 constant PC_DRONE = 8;
+    uint256 constant PC_SUITE = 7;
+    uint256 constant PC_BOT = 6;
+    uint256 constant PC_GENOME = 5;
+    uint256 constant PC_WEAPON = 4;
+    uint256 constant PC_STAR = 3;
+    uint256 constant PC_EQUIPMENT = 2;
+    uint256 constant PC_RESERVED1 = 1;
+    uint256 constant PC_RESERVED2 = 0;
 
     function setup(address nftCoreV1, address nftCoreV2)
         public
@@ -25,6 +39,43 @@ contract Ecio2EpicWarriorsCollection is EcioChallenge {
         address[] memory contracts,
         uint256[] memory tokenIds
     ) public view virtual override returns (uint256) {
-       return 200;
+
+        uint count;
+        
+        for (uint256 index = 0; index < tokenIds.length; index++) {
+            
+            uint256 tokenId = tokenIds[index];
+            address contractAdress = contracts[index];
+
+            string memory partCode;
+            (partCode, ) = IECIONFT(contractAdress).tokenInfo(tokenId);
+
+            string[] memory splittedPartCodes = splitPartCode(partCode);
+            string memory headCode = splittedPartCodes[PC_GENOME];
+
+            if (
+                compareStrings(headCode, "13") ||
+                compareStrings(headCode, "14") ||
+                compareStrings(headCode, "15") ||
+                compareStrings(headCode, "16")
+            ) {
+                count++;
+
+                if(count >= 2){
+                    return 20000 * 1e18;
+                }
+            }
+        }
+
+        return  0;
+    }
+
+    function compareStrings(string memory a, string memory b)
+        public
+        pure
+        returns (bool)
+    {
+        return (keccak256(abi.encodePacked((a))) ==
+            keccak256(abi.encodePacked((b))));
     }
 }
