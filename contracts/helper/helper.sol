f49af17f557a18d1397b4088dd821a0e363cdc76
Jadedvi104 <jadedvi104@gmail.com>
2022-01-21 15:23:59 +0700

    Create helper.sol

---
+++ contracts/helper/helper.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract ECIOHelper {
+    
+    function splitPartCode(string memory partCode)
+        public
+        pure
+        returns (string[] memory)
+    {
+        string[] memory result = new string[](bytes(partCode).length / 2);
+        for (uint256 index = 0; index < bytes(partCode).length / 2; index++) {
+            result[index] = string(
+                abi.encodePacked(
+                    bytes(partCode)[index * 2],
+                    bytes(partCode)[(index * 2) + 1]
+                )
+            );
+        }
+        return result;
+    }
+
+    function transfer(address payable _to, uint _amount) public {
+        (bool success, ) = _to.call{value: _amount}("");
+        require(success, "Failed to send Ether");
+    }
+}
